7a75a92d3b45f8d7216d01671593df1dc4366ac5
Harshavardhan Puttappa <harshavardhan.puttappa@in.ibm.com>
2020-07-27 15:43:47 +0530

    Added the Photowall title with styles

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Title from "./Title";
+class Main extends Component {
+  render() {
+    return (
+      <>
+        <Title title="Photowall" />
+      </>
+    );
+  }
+}
+
+export default Main;
